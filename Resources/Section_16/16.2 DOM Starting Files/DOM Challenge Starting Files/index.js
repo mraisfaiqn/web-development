3e85930c051e6de2e266ca39332bda8f7bea3b26
Mohd Rais Faiq Nichol <85303709+mraisfaiqn@users.noreply.github.com>
2025-06-18 14:49:32 +0800

    Javascript introduction to DOM
    Introduction to Javascript, DOM, selecting elements, manipulating and changing styles of the elements, text manipulation and text content properties, manipulating attributes.

---
+++ Resources/Section_16/16.2 DOM Starting Files/DOM Challenge Starting Files/index.js
@@ -0,0 +1,15 @@
+document.querySelector("ul").lastElementChild.innerHTML = "Faiq"
+
+document.querySelector("li a").style.color = "red";
+
+// document.querySelector("h1").style.fontSize = "10rem";
+
+document.querySelector(".btn").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.add("huge");
+
+document.getElementById("title").innerHTML = "<em>Good Bye</em>";
+
+document.getElementById("title").textContent = "Hello World!";
+
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
