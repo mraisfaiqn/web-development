24cd5eb6f5b1daa17efb92400629c1dac67bf44c
Mohd Rais Faiq Nichol <85303709+mraisfaiqn@users.noreply.github.com>
2026-01-13 19:49:33 +0800

    Updated resources file naming

---
+++ Resources/20. The Simon Game/20.1 Simon Game Challenge Starting Files/jQuery.js
@@ -0,0 +1,108 @@
+// function buttonAnimation(colour){
+// 	var activeButton = $("#" + colour);
+// 	activeButton.addClass("pressed");
+// 	setTimeout(function() {
+// 		activeButton.removeClass("pressed");
+// 	}, 100);
+// }
+
+// function soundControl(button) {
+// 	switch (button) {
+// 		case 'red':
+// 			var redButton = new Audio("./sounds/red.mp3");
+// 			redButton.play();
+// 		break;
+
+// 		case 'blue':
+// 			var blueButton = new Audio("./sounds/blue.mp3");
+// 			blueButton.play();
+// 		break;
+
+// 		case 'green':
+// 			var greenButton = new Audio("./sounds/green.mp3");
+// 			greenButton.play();
+// 		break;
+
+// 		case 'yellow':
+// 			var yellowButton = new Audio("./sounds/yellow.mp3");
+// 			yellowButton.play();
+// 		break;
+
+// 		default: console.log(key);
+
+// 	}
+// }
+
+// $("button").remove();
+
+// $("body").addClass("reset-border");
+// $("h1").addClass("big-title set-font");
+
+// $("h1").text("This is my Website");
+// $("button").text("Don't Click!");
+// $("button").html("<em>Dont Click!</em>");
+
+// $("img").attr("src", "./images/image2.png");
+// $("a").attr("href", "https://www.yahoo.com");
+
+// $("body").keydown(function(event) {
+// 	console.log(event.key);
+// 	$("h1").text(event.key);
+// })
+
+// // $("h1").on("mouseover", function() {
+// // 	$("h1").html("<s>This is my Website</s>");
+// // })
+
+// $("h1").before("<button>New</button");
+// $("h1").after("<button>New</button");
+// $("h1").prepend("<button>New</button");
+// $("h1").append("<button>New</button");
+
+// $("button").click(function() {
+// 	$("h1").css("color", "purple");
+// })
+
+// $("h1").click(function() {
+// 	$("h1").css("color", "black");
+// })
+
+// // $("button").on("click", function() {
+// // 	$("h1").hide();
+// // })
+
+// // $("button").on("click", function() {
+// // 	$("h1").show();
+// // })
+
+// // $("button").on("click", function() {
+// // 	$("h1").toggle();
+// // })
+
+// // $("h1").on("click", function() {
+// // 	$("h1").fadeOut();
+// // })
+
+// // $("button").on("click", function() {
+// // 	$("h1").fadeIn();
+// // })
+
+// // $("button").on("click", function() {
+// // 	$("h1").fadeToggle();
+// // })
+
+// // $("button").on("click", function() {
+// // 	$("h1").slideUp();
+// // })
+
+// // $("button").on("click", function() {
+// // 	$("h1").slideDown();
+// // })
+
+// // $("button").on("click", function() {
+// // 	$("h1").animate({opacity: 0.5});
+// // })
+
+// $("button").on("click", function() {
+// 	$("h1").slideUp().slideDown().animate({opacity: 0.5});
+// })
